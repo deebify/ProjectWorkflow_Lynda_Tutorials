7332b82ce5bfb6dbd35f85783126108d4eee6a55
deebify <yassine.deeb@gmail.com>
2015-11-21 14:20:49 +0200

    fist commit -- Workflow test project locally

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by deeb on 11/21/15.
+ */
+var app = app | {};
+app.prototype.test = function () {
+    console.log('Test Function ');
+};
+app.prototype.app = function (name,age) {
+    this.name = name;
+    this.age = age;
+};
+app.prototype.toString = function(){
+    console.log(this.name+" "+this.age);
+};
+app.name = 'yassine';
+app.age = 21;
+app.toString();
+
